31bcadbb4cd6db092ec4da8d8b0388c4f76122f4
shawn lohstroh <shawn.lohstroh@modopayments.com>
2020-07-16 23:12:34 -0500

    Initial Code Commit

---
+++ Modo2Auth.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Modo
+{
+    public class Modo2Auth
+    {
+        private const String cHeaderPlain = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}";
+        private String _header;
+        private HMAC _hmac;
+        private SHA256 _shaGenerator;
+        private String _apiId;
+        private DateTime _epochDate = new DateTime(1970, 1, 1);
+
+        public Modo2Auth(String apiSecret, String apiId)
+        {
+            byte[] keyBytes = Encoding.ASCII.GetBytes(apiSecret);
+            _hmac = new HMACSHA256(keyBytes);
+            _shaGenerator = SHA256.Create();
+
+            _apiId = apiId;
+            _header = Convert.ToBase64String(Encoding.ASCII.GetBytes(cHeaderPlain)).TrimEnd('=');
+        }
+
+        private String createSignature(String payload)
+        {
+            String message = String.Format("{0}.{1}", _header, payload);
+            byte[] message_bytes = Encoding.ASCII.GetBytes(message);
+
+            byte[] hashBytes = _hmac.ComputeHash(message_bytes);
+
+            String signature = Convert.ToBase64String(hashBytes);
+            signature = signature.TrimEnd('=').Replace('+', '-').Replace('/', '_');
+            return signature;
+        }
+
+        public String createModoToken(String uri, String body)
+        {
+            // Get a SHA of the request body 
+            byte[] hashBytes = _shaGenerator.ComputeHash(Encoding.ASCII.GetBytes(body));
+            String bodySha = BitConverter.ToString(hashBytes).Replace("-","").ToLower();
+
+            // Current time in seconds since Epoch
+            TimeSpan t = DateTime.UtcNow - _epochDate;
+            int secondsSinceEpoch = (int)t.TotalSeconds;
+
+            // create the payload base64 string (trimmed)
+            String payloadPlain = String.Format("{{\"iat\":{0},\"api_identifier\":\"{1}\",\"api_uri\":\"{2}\",\"body_hash\":\"{3}\"}}", secondsSinceEpoch, _apiId, uri, bodySha);
+            String payload = Convert.ToBase64String(Encoding.ASCII.GetBytes(payloadPlain)).TrimEnd('=');
+
+            // Sign it with the API secret
+            String signature = createSignature(payload);
+
+            // put it all toegether and create a token
+            String modoAuthToken = String.Format("MODO2 {0}.{1}.{2}", _header, payload, signature);
+
+            return modoAuthToken;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+
+namespace Modo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String apiId = "Add Api ID here";
+            String apiSecret = "Add Api Secret here";
+
+            String apiUri = "/v2/reports";
+            String fullUri = "https://checkout.mtktest.modopayments.net/v2/reports";
+
+            String requestBody =  "{\"start_date\": \"2020-07-13T00:00:00Z\",\"end_date\": \"2020-07-13T23:59:59Z\"}";
+
+
+            Modo2Auth auth = new Modo2Auth(apiSecret, apiId);
+            String authToken = auth.createModoToken(apiUri, requestBody);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(fullUri);
+            request.ContentType = "application/json";
+            request.Method = WebRequestMethods.Http.Post;
+
+            request.Headers.Add("Authorization", authToken);
+
+            using (StreamWriter streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(requestBody);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            String result = "";
+            using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
